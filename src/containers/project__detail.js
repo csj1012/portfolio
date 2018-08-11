52185b6fab1e95a5c25386859d89d0a4215e95e1
Chelsie Johnston <chels@Chelsies-MacBook-Pro.local>
2018-08-11 14:47:02 -0500

    Rearrange some things.

--- src/containers/project__detail.js
+++ src/containers/project__detail.js
@@ -8,16 +8,27 @@ class ProjectDetail extends Component {
   render() {
     const { id } = this.props.match.params;
 
-    const post = getPost(id);
-    console.log(post);
+    // const post = getPost(id);
+    // console.log(post);
 
     const projectData = this.props.project.projects.find(e => e.id === id);
 
     console.log(projectData);
 
     return (
-      <section className="portfolio__project-detail">        
-        <p>Viewing project "{projectData.title}"</p>
+      <section className="portfolio__project-detail">
+        <a href="/" className="portfolio__project-detail__banner">
+          <img src="https://loremflickr.com/640/360" />
+          <span>Texty text text.</span>
+        </a>
+
+        <p>More info about {projectData.title}</p>
+
+        <ul>
+          <li><a href="/">Project link one</a></li>
+          <li><a href="/">Project link two</a></li>
+          <li><a href="/">Project link three</a></li>
+        </ul>
 
       </section>
     );
