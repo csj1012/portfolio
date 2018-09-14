f2c74129b5c0a85b17d0ec8a23a37effef986804
Chelsie Johnston <chels@Chelsies-MacBook-Pro.local>
2018-09-14 11:56:51 -0500

    Add some content and stuff.

--- src/reducers/reducer_projects.js
+++ src/reducers/reducer_projects.js
@@ -1,16 +1,23 @@
 export default function() {
   return [
     {
-      title: 'AMA Topic Pages',
+      organization: 'American Medical Association',
+      title: 'Topic Pages',
+      shortDescription: 'Drupal 8 minisites to act as a hub for the AMA to promote specialized content using AMA Style Guide elements',
       image: {
-        src: 'colors1.png',
+        src: 'topicpage.png',
         alt: 'Image alt.',
         caption: 'Test caption one.'
       },
       techs: [
-        'drupal',
-        'bloopal'
+        'Drupal',
+        'PatternLab',
+        'Twig',
+        'SCSS',
+        'Gulp',
+        'PHP'
       ],
+      role: 'Drupal development, front end development',
       description: 'Glorpy glorp glorp, I\'m a description.',
       links: [
         {
@@ -28,7 +35,16 @@ export default function() {
       ]
     },
     {
-      title: 'AMA Style Guides',
+      organization: 'American Medical Association',
+      title: 'Style Guides',
+      shortDescription: 'PatternLab suite showcasing reusable atomic design elements to be used throughout the AMA’s digital products, including a custom Drupal 8 theme',
+      techs: [
+        'PatternLab',
+        'Twig',
+        'SCSS',
+        'Gulp',
+        'Atomic design'
+      ],
       image: {
         src: 'colors1.png',
         alt: 'Image alt.',
@@ -37,6 +53,8 @@ export default function() {
       caption: 'Test caption two.'
     },
     {
+      organization: 'University of Chicago',
+      shortDescription: 'D6 to D7 migration project encompassing a total website redesign and major content reorganization',
       title: 'UChicago News',
       image: {
         src: 'colors1.png',
@@ -46,7 +64,9 @@ export default function() {
       caption: 'Test caption three.'
     },
     {
+      organization: 'University of Chicago',
       title: 'UChicago MAPPSS',
+      shortDescription: 'Customizable and themable Drupal boilerplate site for the UChicago MAPPSS program',
       image: {
         src: 'colors1.png',
         alt: 'Image alt.',
@@ -55,7 +75,9 @@ export default function() {
       caption: 'Test caption four.'
     },
     {
+      organization: 'University of Chicago',
       title: 'UChicago Sites',
+      shortDescription: 'Multisite Drupal suite used to house websites for over 400 University organizations',
       image: {
         src: 'colors1.png',
         alt: 'Image alt.',
@@ -70,6 +92,7 @@ export default function() {
         alt: 'Image alt.',
         caption: 'Test caption one.'
       },
+      shortDescription: 'Homebrew role-playing game built using ExpressionEngine, custom PHP modules, and lots of love',
       caption: 'Test caption four.'
     },
   ]
