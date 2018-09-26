72c06dadb3b6424f77a09923d3a5eec43a48322d
Chelsie Johnston <chels@Chelsies-MacBook-Pro.local>
2018-09-26 17:42:59 -0500

    Add a subnav.

--- src/index.js
+++ src/index.js
@@ -20,8 +20,17 @@ ReactDOM.render(
   <Provider store={store}>
     <div className="portfolio__wrapper">
       <Header />
-      <div className="portfolio__photo">Photo</div>
-      <p className="portfolio__blurb">I'm a web designer and developer. I have lots of experience in Drupal. I created my first website when I was ten years old. I love my cat and I just learned how to knit. I'm pretty depressed. Please use my photo to discriminate against me. Let's get in touch!</p>
+      <section className="portfolio__intro">
+        <div className="portfolio__photo">Photo</div>
+        <p className="portfolio__blurb">I'm a web designer and developer. I have lots of experience in Drupal. I created my first website when I was ten years old. I love my cat and I just learned how to knit. I'm pretty depressed. Please use my photo to discriminate against me. Let's get in touch!</p>
+        <p className="portfolio__links">
+          <ul>
+            <li>My Work</li>
+            <li>About Me</li>
+            <li>Get in Touch?</li>
+          </ul>
+        </p>
+      </section>
       <ProjectDetail />
       <ProjectsList />
     </div>
