dac358063fad3ee97311cca0c5e6a77dd1af3d44
Chelsie Johnston <chels@Chelsies-MacBook-Pro.local>
2018-09-27 16:05:17 -0500

    Hook up the nav.

--- src/containers/nav.js
+++ src/containers/nav.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default class Nav extends Component {
+export class Nav extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
@@ -9,8 +9,7 @@ export default class Nav extends Component {
 
   handleClick(e) {
     // 'test' will eventually hold the thing that is clicked
-    this.props.onNavClick('test');
-    console.log(e);
+    this.props.onNavClick(e.target.dataset.section);
   }
 
 render() {
@@ -19,19 +18,19 @@ render() {
     return (
       <p className="portfolio__links">
         <ul>
-          <li key="work" onClick={this.handleClick}>My Work</li>
-          <li key="about" onClick={this.handleClick}>About Me</li>
-          <li key="contact" onClick={this.handleClick}>Get in Touch?</li>
+          <li data-section="work" onClick={this.handleClick}>My Work</li>
+          <li data-section="about" onClick={this.handleClick}>About Me</li>
+          <li data-section="contact" onClick={this.handleClick}>Get in Touch?</li>
         </ul>
       </p>
     );
   };
 }
-//
-// function mapStateToProps(state) {
-//   return {
-//     selected: state.selected
-//   };
-// }
-//
-// export default connect(mapStateToProps)(Nav);
+
+function mapStateToProps(state) {
+  return {
+    section: state.section
+  };
+}
+
+export default connect(mapStateToProps)(Nav);
