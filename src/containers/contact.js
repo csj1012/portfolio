5cc077fee5fa42eb4e57d03b26a881beb1142e0b
Chelsie Johnston <chels@Chelsies-MacBook-Pro.local>
2018-10-15 19:16:57 -0500

    Add and format resume.

--- src/containers/contact.js
+++ src/containers/contact.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Resume from './resume';
 
 export default class Contact extends Component {
   render() {
     if (this.props.section == 'contact') {
       return (
         <section className="portfolio__contact">
-          CONTACT ME UP
+          <Resume />
         </section>
       );
     } else {
