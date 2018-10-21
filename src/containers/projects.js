9f851d8974106bada11b5a43349297b5bc033663
Chelsie Johnston <chelsiesjohnston@gmail.com>
2018-10-20 20:38:01 -0500

    Add projects wrapper.

---
+++ src/containers/projects.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ProjectsList from './projects__list';
+import ProjectDetail from './project__detail';
+import ProjectsTeaserList from './project__teaser-list';
+
+export default class Projects extends Component {
+  render() {
+    if (this.props.section === 'detail') {
+      return (
+        <div>
+          Project Detail.
+        </div>
+      )
+    }
+
+    if (this.props.section === 'work') {
+      return (
+        <div>
+          Section is {this.props.section}
+          <ProjectsTeaserList />
+        </div>
+      )
+    }
+
+    else {
+      return (
+        <div className="hidden"></div>
+      )
+    }
+  }
+}
