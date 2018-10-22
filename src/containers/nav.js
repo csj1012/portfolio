786ef8b149cfdd462ce72a6c1a94dc55cf727f36
Chelsie Johnston <chelsiesjohnston@gmail.com>
2018-10-21 23:51:21 -0500

    Update all the styling\!

--- src/containers/nav.js
+++ src/containers/nav.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-export class Nav extends Component {
+export default class Nav extends Component {
 render() {
-    const section = this.props.section;
-
     return (
       <div className="portfolio__nav">
         <ul>
-          <li><Link to='/work'>Projects</Link></li>
-          <li><Link to='/about'>About Me</Link></li>
-          <li><Link to='/contact'>Résumé & Contact</Link></li>
+          <li><NavLink to='/work' activeClassName="nav--active">Projects</NavLink></li>
+          <li><NavLink to='/about' activeClassName="nav--active">About Me</NavLink></li>
+          <li><NavLink to='/contact' activeClassName="nav--active">Résumé & Contact</NavLink></li>
         </ul>
       </div>
     );
   };
 }
-
-function mapStateToProps(state) {
-  return {
-    section: state.section
-  };
-}
-
-export default connect(mapStateToProps)(Nav);
