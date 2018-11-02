b137a689fc47c5ab2b3a7a641db721e8df1089bf
Chelsie Johnston <chelsie.summer@gmail.com>
2018-11-02 11:36:09 -0500

    Updates

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export const SELECT_PROJECT = 'select_project';
-
-export function selectProject(project) {
-
-  return {
-    type: SELECT_PROJECT,
-    payload: project
-  }
-}
--- src/reducers/reducer_project-detail.js
+++
@@ -1,9 +0,0 @@
-// State arg is not application state, only the state that this reducer is responsible for.
-export default function(state = null, action) {
-  switch (action.type) {
-    case 'select_project':
-      return action.payload;
-  }
-
-  return state;
-}
